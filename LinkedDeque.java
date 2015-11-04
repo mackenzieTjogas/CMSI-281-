adc34105ed56da6521e8690b5b8e52510942ea98
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2015-11-03 16:09:26 -0800

    Finished LinkedDeque.java

--- LinkedDeque.java
+++ LinkedDeque.java
@@ -1,80 +1,246 @@
 public class LinkedDeque {
     private Node leftEnd;
     private Node rightEnd;
-    public int size;
-    //default constructor
+    private int size;
+    private static int successes = 0;
+    private static int attempts = 0;
+
     public LinkedDeque() {
-        leftEnd = null;
-        rightEnd = null;
-        size = 0;
+    }
+
+    public static LinkedDeque createFrom(Object[] given) {
+        LinkedDeque ret = new LinkedDeque();
+        for (int i = 0; i < given.length; i++) {
+            ret.insertRight(given[i]);
+        }
+        return ret;
     }
 
     public void insertLeft(Object o) {
-        if ((leftEnd == null) && (rightEnd == null)) {
-            Node value = new Node(o);
-            leftEnd = value;
-            rightEnd = value;
+        leftEnd = new Node(o, null, leftEnd);
+        if (size() == 0) {
+            rightEnd = leftEnd;
         } else {
-            Node value = new Node(o, rightEnd);
-            leftEnd.leftNode = value;
-            value.rightNode = leftEnd;
-            leftEnd = value;
+            leftEnd.right().setLeft(leftEnd);
         }
         size++;
     }
 
     public void insertRight(Object o) {
-        if ((leftEnd == null) && (rightEnd == null)) {
-            Node value = new Node(o);
-            leftEnd = value;
-            rightEnd = value;
+        rightEnd = new Node(o, rightEnd, null);
+        if (size() == 0) {
+            leftEnd = rightEnd;
         } else {
-            Node value = new Node(leftEnd, o);
-            rightEnd.rightNode = value;
-            value.leftNode = rightEnd;
-            rightEnd = value;
+            rightEnd.left().setRight(rightEnd);
         }
         size++;
     }
+    
     public void deleteLeft() {
+        if (leftEnd == null) {
+            return;
+        }
         leftEnd = leftEnd.rightNode;
         leftEnd.leftNode = null;
         size--;
     }
+
     public void deleteRight() {
+        if (rightEnd == null) {
+            return;
+        }
         rightEnd = rightEnd.leftNode;
         rightEnd.rightNode = null;
         size--;
     }
+
     public Object left() {
         return leftEnd.getData(); 
     }
+
     public Object right() {
         return rightEnd.getData(); 
     }
+
     public int size() {
         return size;
     }
+
     public String toString() {
         String finalString = "";
-        Node currentElement = rightEnd;
+        Node currentElement = leftEnd;
         while (currentElement != null) {
             finalString += "[" + currentElement.getData() + "]";
-            System.out.println("leftNode: " + currentElement.left() + ", rightNode: " + currentElement.rightNode);
-            System.out.println("////////");
-            currentElement = currentElement.left();
+            currentElement = currentElement.right();
             
         }
         return finalString;
     } 
+
+    public boolean equals(LinkedDeque second) {
+        if (!(second instanceof LinkedDeque)) {
+            return false;
+        }
+        if (this.size() != second.size()) {
+            return false;
+        }
+        Node currentElement = rightEnd;
+        Node otherElement = second.rightEnd;
+        while (currentElement != null) {
+            if (!currentElement.getData().equals(otherElement.getData())){
+                return false;
+            }
+            otherElement = otherElement.left();
+            currentElement = currentElement.left();
+        }
+        return true;
+    }  
+
     public static void main(String[] args) {
-        LinkedDeque test = new LinkedDeque();
-        test.insertRight("dog");
-        test.insertRight("cat");
-        test.insertRight("lamb");
-        test.insertRight("bunny");
-        System.out.println(test);
-        System.out.println(test.size);
+        attempts = 0;
+        successes = 0;
+
+        test_constructors();
+        test_insertLeft();
+        test_insertRight();
+        test_deleteLeft();
+        test_deleteRight();
+        test_left();
+        test_right();
+        test_size();
+        test_toString();
+
+        System.out.println(successes + "/" + attempts + " tests passed.");
+    } 
+
+    private static void displaySuccessIfTrue(boolean value) {
+        attempts++;
+        successes += value ? 1 : 0;
+
+        System.out.println(value ? "successes" : "failure");
+    }
+
+    private static void test_constructors() {
+        System.out.println("Testing constructors...");
+        LinkedDeque deque = new LinkedDeque();
+
+        try {
+            displaySuccessIfTrue(deque.size() == 0);
+        } catch(Exception e) {
+            displaySuccessIfTrue(false);
+        }
+
+        try {
+            displaySuccessIfTrue((deque.leftEnd == null) && (deque.rightEnd == null));
+        } catch(Exception e) {
+            displaySuccessIfTrue(false);
+        }
+    }
+
+    private static void test_insertLeft() {
+        System.out.println("Testing insertLeft...");
+        LinkedDeque deque1 = new LinkedDeque();
+        deque1.createFrom(new Integer[] {1,2, 3});
+        deque1.insertLeft(4);
+
+        try {
+            displaySuccessIfTrue(deque1.leftEnd.getData().equals(new Integer(4)));
+        } catch(Exception e) {
+            displaySuccessIfTrue(false);
+        }
+
+    }
+
+    private static void test_insertRight() {
+        System.out.println("Testing insertRight...");
+        LinkedDeque deque2 = new LinkedDeque();
+        deque2.createFrom(new String[] {"7" ,"8", "9"});
+        deque2.insertRight("6");
+
+        try {
+            displaySuccessIfTrue(deque2.rightEnd.getData().equals("6"));
+        } catch(Exception e) {
+            displaySuccessIfTrue(false);
+        }
+
+    }
+
+    private static void test_deleteLeft() {
+        System.out.println("Testing deleteLeft...");
+        LinkedDeque deque3 = LinkedDeque.createFrom(new String[] {"4" ,"2", "1"});
+        deque3.deleteLeft();
+        LinkedDeque check = LinkedDeque.createFrom(new String[] {"2", "1"});
+        
+        System.out.println("check: " + check.toString());
+        System.out.println("deque3: " + deque3.toString());
+        
+        try {
+            displaySuccessIfTrue(deque3.equals(check));
+        } catch(Exception e) {
+            displaySuccessIfTrue(false);
+        }
+
+    }
+
+    private static void test_deleteRight() {
+        System.out.println("Testing deleteRight...");
+        LinkedDeque deque4 = LinkedDeque.createFrom(new String[] {"6" ,"18", "4"});
+        deque4.deleteRight();
+        LinkedDeque check2 = LinkedDeque.createFrom(new String[] {"6", "18"});
+        
+        try {
+            displaySuccessIfTrue(deque4.equals(check2));
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }
+
+    }
+
+    private static void test_left() {
+        System.out.println("Testing left...");
+        LinkedDeque deque5 = LinkedDeque.createFrom(new String[] {"7" ,"12", "5"});
+        
+        try {
+            displaySuccessIfTrue(deque5.left().equals("7"));
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }
+
+    }
+
+    private static void test_right() {
+        System.out.println("Testing right...");
+        LinkedDeque deque6 = LinkedDeque.createFrom(new String[] {"7" ,"12", "5"});
+        
+        try {
+            displaySuccessIfTrue(deque6.right().equals("5"));
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }
+
+    }
+
+    private static void test_size() {
+        System.out.println("Testing size...");
+        LinkedDeque deque6 = LinkedDeque.createFrom(new String[] {"7" ,"12", "5"});
+        
+        try {
+            displaySuccessIfTrue(deque6.size() == 3);
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }
+
+    }
+
+    private static void test_toString() {
+        System.out.println("Testing toString...");
+        LinkedDeque deque7 = LinkedDeque.createFrom(new String[] {"7" ,"12", "5"});
+        
+        try {
+            displaySuccessIfTrue(deque7.toString().equals("[7][12][5]"));
+        } catch (Exception e) {
+            displaySuccessIfTrue(false);
+        }
 
     }
 
@@ -85,26 +251,17 @@ public class LinkedDeque {
 
         public Node(Object data) {
             this.data = data;
-            this.leftNode = null;
-            this.rightNode = null;
         }
-
-        public Node(Node leftNode, Object data) {
-            this.data = data;
-            this.leftNode = leftNode;
-            this.rightNode = null;
-        }
-
-        public Node(Object data, Node rightNode) {
-            this.data = data;
-            this.leftNode = null;
-            this.rightNode = rightNode;
-        }
-
-        public Node(Node leftNode, Object data, Node rightNode) {
+        public Node(Object data, Node leftNode, Node rightNode) {
             this.data = data;
             this.leftNode = leftNode;
             this.rightNode = rightNode;
+        }
+        public void setLeft(Node leftN) {
+            leftNode = leftN;
+        }
+        public void setRight(Node rightN) {
+            rightNode = rightN;
         }
 
         public Object getData() {
@@ -112,6 +269,9 @@ public class LinkedDeque {
         }
         public Node left() {
             return this.leftNode;
+        }
+        public Node right() {
+            return this.rightNode;
         }
     }
 
